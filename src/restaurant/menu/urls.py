46ca1fd2cd42f137842b93569902b4dab9c49da6
Mateusz Moneta <mateuszm@opera.com>
2016-11-25 08:56:00 +0100

    Move api urls to /api path.

--- src/restaurant/menu/urls.py
+++ src/restaurant/menu/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import url
+from django.views.defaults import page_not_found
 
 from restaurant.menu.views import IndexView, MenusView, DishesPerMenuView
 
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='menu-index'),
-    url(r'^list$', MenusView.as_view(), name='menu-list'),
+]
+
+apipatterns = [
+    url(r'^$', page_not_found, name='menu-api'),
+    url(r'^list$', MenusView.as_view(), name='menu-api-list'),
     url(r'^(?P<pk>\d+)/dishes$', DishesPerMenuView.as_view(),
-        name='menu-dishes'),
+        name='menu-api-dishes'),
 ]
--- src/restaurant/urls.py
+++ src/restaurant/urls.py
@@ -3,13 +3,17 @@ from django.conf import settings
 from django.contrib import admin
 
 from restaurant.views import IndexView
-from restaurant.menu import urls as menu_urls
+from restaurant.menu.urls import (
+    urlpatterns as menu_urls,
+    apipatterns as api_urls,
+)
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('^$', IndexView.as_view()),
     url(r'^menu/', include(menu_urls)),
+    url(r'^api/menu/', include(api_urls)),
 ]
 urlpatterns += static.static(settings.MEDIA_URL,
                              document_root=settings.MEDIA_ROOT)
