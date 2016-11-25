2d99ff0c6af4f1fecb46616bca9f55a32c98b876
Mateusz Moneta <mateuszm@opera.com>
2016-11-25 08:25:08 +0100

    Fix caching xhr calls.

--- src/restaurant/menu/static/js/services/apiService.js
+++ src/restaurant/menu/static/js/services/apiService.js
@@ -17,7 +17,10 @@
     var menusCache = CacheFactory.get('menusCache');
     var dishesCache = CacheFactory.get('dishesCache');
 
-    var handleResponse = function (data, params) {
+    function handleResponse (data, params) {
+    /**
+     * Handle paginated and regular responses.
+     */
       if (data.next) {
         params.successCallback(data.results);
         params.url = data.next;
@@ -25,11 +28,15 @@
       } else {
         params.successCallback(data.results || data);
       }
-    };
+    }
 
-    var doRequest = function (params, base_url, cache) {
+    function doRequest(params, base_url)
+    /**
+     * Make get requests to retrieve data from the API. Display progress bar.
+     */
+    {
       NProgress.start();
-      return $http.get(params.url || base_url, {cache: cache,
+      return $http.get(params.url || base_url, {cache: params.cache,
           timeout: timeout}).then(
             function (response) {
           if (response.status === 200) {
@@ -39,14 +46,16 @@
           }
           NProgress.done();
         }, params.errorCallback);
-      };
+      }
 
     return {
       getMenus: function (params) {
-        return doRequest(params, getMenusUrl, menusCache);
+        params.cache = menusCache;
+        return doRequest(params, getMenusUrl);
       },
       getDishesFromMenu: function (params) {
-        return doRequest(params, apiUrl + params.menu.id + '/dishes', dishesCache);
+        params.cache = dishesCache;
+        return doRequest(params, apiUrl + params.menu.id + '/dishes');
       }
     }
   }]);
